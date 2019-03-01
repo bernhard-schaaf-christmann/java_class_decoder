c562679c29f61f13c77a84ca4ef9afc334ed421c
Bernhard Schaaf-Christmann <brs@dialogika.de>
2019-03-01 20:51:46 +0100

    Read whole class file. Doesn' t decode attributes.

--- src/decode.cpp
+++ src/decode.cpp
@@ -168,6 +168,83 @@ public:
 		}
 		std::cout << '"' << std::endl;
 	}
+	enum ACC {
+		PUBLIC = 0x0001,
+		FINAL = 0x0010,
+		SUPER = 0x0020,
+		INTERFACE = 0x0200,
+		ABSTRACT = 0x0400
+	};
+	void flags_and_classes() {
+		uint16_t access_flags = next_u2();
+		uint16_t this_class = next_u2();
+		uint16_t super_class = next_u2();
+		std::cout << "access_flags " << std::hex << access_flags << std::dec << " – ";
+		if (ACC::PUBLIC & access_flags) std::cout << "public ";
+		if (ACC::FINAL & access_flags) std::cout << "final ";
+		if (ACC::SUPER & access_flags) std::cout << "super ";
+		if (ACC::INTERFACE & access_flags) std::cout << "interface ";
+		if (ACC::ABSTRACT & access_flags) std::cout << "abstract ";
+		std::cout << std::endl;
+		std::cout << "this_class " << this_class << std::endl;
+		std::cout << "super_class " << super_class << std::endl;
+	}
+	void interfaces() {
+		uint16_t length = next_u2();
+		std::cout << "Interfaces length: " << length << std::endl;
+		for (uint16_t index = 0; index < length; ++index) {
+			interface(index);
+		}
+	}
+	void interface(uint16_t index) {
+		uint16_t that_interface = next_u2();
+		std::cout << '@' << index << " interface: " << that_interface << std::endl;
+	}
+	void fields() {
+		uint16_t length = next_u2();
+		std::cout << "Fields length: " << length << std::endl;
+		for (uint16_t index = 0; index < length; ++index) {
+			field_info(index);
+		}
+	}
+	void field_info(uint16_t index) {
+		uint16_t access_flags = next_u2();
+		uint16_t name_index = next_u2();
+		uint16_t descriptor_index = next_u2();
+		std::cout << '@' << index << " field_info - access_flags " << access_flags << " name_index " << name_index << " descriptor_index " << descriptor_index << std::endl;
+		attributes("field_info");
+	}
+	void attributes(const char* belongs) {
+		uint16_t length = next_u2();
+		std::cout << belongs << " – attributes length: " << length << std::endl;
+		for (uint16_t index = 0; index < length; ++index) {
+			attribute_info(index);
+		}
+	}
+	void attribute_info(uint16_t index) {
+		uint16_t attribute_name_index = next_u2();
+		uint32_t length = next_u4();
+		std::cout << '@' << index << " attribute_info - attribute_name_index " << attribute_name_index << " length " << length << std::endl;
+		std::cout << std::hex;
+		for (uint32_t index = 0; index < length; ++index) {
+			std::cout << next_u1() << ' '; // TODO handle attributes: constants, code, exception
+		}
+		std::cout << std::dec << std::endl;
+	}
+	void methods() {
+		uint16_t length = next_u2();
+		std::cout << "methods length: " << length << std::endl;
+		for (uint16_t index = 0; index < length; ++index) {
+			method_info(index);
+		}
+	}
+	void method_info(uint16_t index) {
+		uint16_t access_flags = next_u2();
+		uint16_t name_index = next_u2();
+		uint16_t descriptor_index = next_u2();
+		std::cout << '@' << index << " method_info - access_flags " << access_flags << " name_index " << name_index << " descriptor_index " << descriptor_index << std::endl;
+		attributes("method_info");
+	}
 private:
 	std::string filename_;
 	std::fstream classfile_;
@@ -179,6 +256,11 @@ int main() {
 	from.magic();
 	from.version();
 	from.constant_pool();
+	from.flags_and_classes();
+	from.interfaces();
+	from.fields();
+	from.methods();
+	from.attributes("class");
 	return 0;
 }
 
